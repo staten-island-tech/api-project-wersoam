d44ee108596f78961d37eac6ae271a6ded909db9
wersoam <cowistrash@gmail.com>
2023-01-10 10:13:15 -0500

    commit

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,18 +1,30 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
 const dom = {
   input: document.querySelector("#input"),
   image: document.getElementById("image"),
   button: document.getElementById("button"),
+  text: document.getElementById("text"),
 };
-// image = "http://yugiohprices.com/api/card_image/${dom.input}";
-// data = "https://yugiohprices.com/api/card_data/${dom.input}";
+const card_image =
+  "http://yugiohprices.com/api/card_image/witchcrafter madame verre";
+const card_effect =
+  "https://yugiohprices.com/api/card_data/witchcrafter madame verre";
 
+async function get(card_effect) {
+  try {
+    const response = await fetch(card_effect);
+    const data = await response.json();
+    dom.text.textContent = data.Content;
+  } catch (error) {
+    console.log(error);
+  }
+}
+get(card_image);
 dom.button.addEventListener("click", function () {
   console.log(dom.input);
   dom.image.insertAdjacentHTML(
     "beforeend",
-    `<img src="http://yugiohprices.com/api/card_image/${dom.input}"><p>"https://yugiohprices.com/api/card_data/${dom.input}"</p>`
+    `<img src=${imagelink}><p>"https://yugiohprices.com/api/card_data/${dom.input}"</p>`
   );
 });
+const imagelink = `http://yugiohprices.com/api/card_image/${dom.input}`;
