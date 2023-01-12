c528425b6a16b3f9f23d9e664530f43adf680017
wersoam <cowistrash@gmail.com>
2023-01-12 10:16:07 -0500

    save

--- vite-project/main.js
+++ vite-project/main.js
@@ -8,18 +8,27 @@ const dom = {
 const card_image =
   "http://yugiohprices.com/api/card_image/witchcrafter madame verre";
 const card_effect =
-  "https://yugiohprices.com/api/card_data/witchcrafter madame verre";
+  "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Tornado%20Dragon";
 
-async function get(card_effect) {
+async function getinfo(card_effect) {
   try {
     const response = await fetch(card_effect);
     const data = await response.json();
-    dom.text.textContent = data.Content;
+    console.log(data);
+    return data;
   } catch (error) {
     console.log(error);
   }
 }
-get(card_image);
+getinfo(card_effect);
+const inserteffect = async () => {
+  // defining an async arrow function
+  const moneff = await fetchData(getinfo);
+  dom.text.innerHTML = `effect: ${moneff.data.desc}`;
+};
+
+inserteffect();
+
 dom.button.addEventListener("click", function () {
   console.log(dom.input);
   dom.image.insertAdjacentHTML(
