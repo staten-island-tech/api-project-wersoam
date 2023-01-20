9723dd0df3d96aad2b4552e60a7196ba849dbac6
wersoam <cowistrash@gmail.com>
2023-01-20 10:14:39 -0500

    more user friendly api found

--- vite-project/main.js
+++ vite-project/main.js
@@ -2,40 +2,30 @@ import "./style.css";
 const dom = {
   input: document.querySelector("#input"),
   image: document.getElementById("image"),
-  button: document.getElementById("button"),
+  button: document.getElementById("btn"),
   text: document.getElementById("text"),
 };
-
-const card_image =
-  "http://yugiohprices.com/api/card_image/witchcrafter madame verre";
-const card_effect =
-  "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=gunkan%20suship%20shari%20red";
+const quote = "https://animechan.vercel.app/api/random/character?name=king";
 
 async function getinfo() {
   try {
-    const response = await fetch(card_effect);
+    const response = await fetch(quote);
     const data = await response.json();
-    dom.text.textContent = data.desc;
+    dom.image.innerHTML = `${data.character}: ${data.quote}`;
     console.log(data);
     return data;
   } catch (error) {
     console.log(error);
   }
 }
-let newarr = [];
 
-const inserteffect = async () => {
-  const moneff = await getinfo();
-  dom.image.innerHTML = `effect: ${moneff.data.desc}`;
-};
+// const inserteffect = async () => {
+//   const moneff = await getinfo();
+//   dom.image.innerHTML = `effect: ${moneff.data.desc}`;
+// };
 
-inserteffect();
+getinfo();
 
 dom.button.addEventListener("click", function () {
-  console.log(dom.input.value);
-  dom.image.insertAdjacentHTML(
-    "beforeend",
-    `<img src=${imagelink}><p>"https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${dom.input.value}"</p>`
-  );
+  getinfo();
 });
-const imagelink = `http://yugiohprices.com/api/card_image/${dom.input.value}`;
