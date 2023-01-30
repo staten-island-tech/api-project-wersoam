02c634c4572728cd57be0fead214d7ebe3c838bc
wersoam <cowistrash@gmail.com>
2023-01-30 17:04:56 -0500

    something does work

--- vite-project/main.js
+++ vite-project/main.js
@@ -5,15 +5,12 @@ const dom = {
   button: document.getElementById("btn"),
   text: document.getElementById("text"),
 };
-const card =
-  "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=gunkan%20suship%20shari%20red";
-
+const card = `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${dom.input.textContent}`;
 async function getinfo() {
   try {
     const response = await fetch(card);
     const info = await response.json();
-
-    dom.image.innerHTML = JSON.stringify(info.data);
+    console.log(info);
     return info;
   } catch (error) {
     console.log(error);
@@ -21,11 +18,18 @@ async function getinfo() {
 }
 async function inserteffect() {
   const moneff = await getinfo();
-  dom.image.innerHTML = `effect: ${moneff.data.desc}`;
+  console.log(moneff);
+  dom.image.insertAdjacentHTML(
+    "beforeend",
+    `<img src="${moneff.data[0].card_images[0].image_url}" >
+  <p>effect: ${moneff.data[0].desc}</p>`
+  );
 }
-
-getinfo();
-
-dom.button.addEventListener("click", function () {
+dom.button.addEventListener(`click`, function () {
+  clear();
   inserteffect();
 });
+inserteffect();
+const clear = function () {
+  dom.image.innerHTML = "";
+};
